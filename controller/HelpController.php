e3a6a83acd34849c84ea123f2c5dd90ada1114af
MAC <gylinuxer@gmail.com>
2020-03-17 08:46:52 +0800

    0317

---
+++ controller/HelpController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liguangyao
+ * Date: 2020/3/17
+ * Time: 08:30
+ */
+namespace app\index\controller;
+Class HelpController extends \think\Controller{
+    public function GetInnerURL(){
+        $server_hostname=gethostname();
+        $server_hostname .= ".";
+        $server_ip=gethostbyname($server_hostname);
+        echo '以下是系统的内部地址,仅限于安装本系统的办公室才可以访问。</br>';
+        print('http://'.$server_ip.'/index');
+    }
+}
---
+++ controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\index\controller;
+
+class Index extends PublicController
+{
+    public function index($id='')
+    {
+        return view("");
+     }
+    public function hello()
+    {
+
+    }
+
+    public function UpdateStoreQKByQKTable(){
+      $rows =  db()->query('SELECT * FROM OtherQFLog');
+      if(empty($rows)){
+          return '无任何欠款记录';
+      }
+      $i=0;
+      foreach ($rows as $r){
+          db()->query("UPDATE StoreList SET OtherQK = OtherQK + ? WHERE StoreCode = ?",[$r['Fee'],$r['StoreCode']]);
+          $i++;
+      }
+      return '更新'.$i.'条记录!';
+    }
+}
